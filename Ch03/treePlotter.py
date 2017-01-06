4f37a6a2068627454e21174a17a82dbac1b92f80
Gray <liu5205007@163.com>
2017-01-06 17:35:02 +0800

    ch03 学习~

--- Ch03/treePlotter.py
+++ Ch03/treePlotter.py
@@ -1,14 +1,18 @@
+#-*- coding: utf-8 -*-，
+#coding = utf-8
 '''
 Created on Oct 14, 2010
 
 @author: Peter Harrington
 '''
 import matplotlib.pyplot as plt
+import trees
 
 decisionNode = dict(boxstyle="sawtooth", fc="0.8")
 leafNode = dict(boxstyle="round4", fc="0.8")
 arrow_args = dict(arrowstyle="<-")
 
+#获取叶节点数目
 def getNumLeafs(myTree):
     numLeafs = 0
     firstStr = myTree.keys()[0]
@@ -19,6 +23,7 @@ def getNumLeafs(myTree):
         else:   numLeafs +=1
     return numLeafs
 
+#获取树的层数
 def getTreeDepth(myTree):
     maxDepth = 0
     firstStr = myTree.keys()[0]
@@ -34,7 +39,8 @@ def plotNode(nodeTxt, centerPt, parentPt, nodeType):
     createPlot.ax1.annotate(nodeTxt, xy=parentPt,  xycoords='axes fraction',
              xytext=centerPt, textcoords='axes fraction',
              va="center", ha="center", bbox=nodeType, arrowprops=arrow_args )
-    
+
+#在父节点间填充文本信息
 def plotMidText(cntrPt, parentPt, txtString):
     xMid = (parentPt[0]-cntrPt[0])/2.0 + cntrPt[0]
     yMid = (parentPt[1]-cntrPt[1])/2.0 + cntrPt[1]
@@ -64,7 +70,7 @@ def createPlot(inTree):
     fig.clf()
     axprops = dict(xticks=[], yticks=[])
     createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)    #no ticks
-    #createPlot.ax1 = plt.subplot(111, frameon=False) #ticks for demo puropses 
+    #createPlot.ax1 = plt.subplot(111, frameon=False) #ticks for demo puropses
     plotTree.totalW = float(getNumLeafs(inTree))
     plotTree.totalD = float(getTreeDepth(inTree))
     plotTree.xOff = -0.5/plotTree.totalW; plotTree.yOff = 1.0;
@@ -85,4 +91,11 @@ def retrieveTree(i):
                   ]
     return listOfTrees[i]
 
-#createPlot(thisTree)
+if __name__ == "__main__":
+    '''
+    myTree = retrieveTree(1)
+    print myTree
+    createPlot(myTree)
+    '''
+    lensesTree = trees.grabTree('lensensStorage.txt')
+    createPlot(lensesTree)
--- Ch03/trees.py
+++ Ch03/trees.py
@@ -1,3 +1,5 @@
+#-*- coding: utf-8 -*-，
+#coding = utf-8
 '''
 Created on Oct 12, 2010
 Decision Tree Source Code for Machine Learning in Action Ch. 3
@@ -16,9 +18,11 @@ def createDataSet():
     #change to discrete values
     return dataSet, labels
 
+#计算给定数据集的香农熵
 def calcShannonEnt(dataSet):
     numEntries = len(dataSet)
     labelCounts = {}
+    #为所有可能分类创建字典
     for featVec in dataSet: #the the number of unique elements and their occurance
         currentLabel = featVec[-1]
         if currentLabel not in labelCounts.keys(): labelCounts[currentLabel] = 0
@@ -28,19 +32,21 @@ def calcShannonEnt(dataSet):
         prob = float(labelCounts[key])/numEntries
         shannonEnt -= prob * log(prob,2) #log base 2
     return shannonEnt
-    
+
+#按照给定特征划分数据集
 def splitDataSet(dataSet, axis, value):
     retDataSet = []
     for featVec in dataSet:
         if featVec[axis] == value:
             reducedFeatVec = featVec[:axis]     #chop out axis used for splitting
-            reducedFeatVec.extend(featVec[axis+1:])
+            reducedFeatVec.extend(featVec[axis+1:]) #此处注意 extend 和append 的区别
             retDataSet.append(reducedFeatVec)
     return retDataSet
-    
+
+#选择最好的数据划分方式
 def chooseBestFeatureToSplit(dataSet):
     numFeatures = len(dataSet[0]) - 1      #the last column is used for the labels
-    baseEntropy = calcShannonEnt(dataSet)
+    baseEntropy = calcShannonEnt(dataSet)  #计算了整个数据集的原始香农熵
     bestInfoGain = 0.0; bestFeature = -1
     for i in range(numFeatures):        #iterate over all the features
         featList = [example[i] for example in dataSet]#create a list of all the examples of this feature
@@ -56,6 +62,7 @@ def chooseBestFeatureToSplit(dataSet):
             bestFeature = i
     return bestFeature                      #returns an integer
 
+#返回出现次数最多的分类名称
 def majorityCnt(classList):
     classCount={}
     for vote in classList:
@@ -64,10 +71,11 @@ def majorityCnt(classList):
     sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
+#创建树
 def createTree(dataSet,labels):
     classList = [example[-1] for example in dataSet]
     if classList.count(classList[0]) == len(classList): 
-        return classList[0]#stop splitting when all of the classes are equal
+        return classList[0]#stop splitting when all of the classes are equal 类别完全相同则停止继续划分
     if len(dataSet[0]) == 1: #stop splitting when there are no more features in dataSet
         return majorityCnt(classList)
     bestFeat = chooseBestFeatureToSplit(dataSet)
@@ -102,4 +110,19 @@ def grabTree(filename):
     import pickle
     fr = open(filename)
     return pickle.load(fr)
-    
+
+def lenses():
+    fr = open('lenses.txt')
+    lenses = [inst.strip().split('\t') for inst in fr.readlines()]
+    lensesLabels = ['age', 'prescript', 'astigmatic', 'tearRate']
+    lensesTree = createTree(lenses,lensesLabels)
+    storeTree(lensesTree,'lensensStorage.txt')
+
+if __name__ == "__main__":
+    #myDat,lables = createDataSet()
+    #print myDat
+    #print splitDataSet(myDat, 0, 1)
+    #print splitDataSet(myDat, 0, 0)
+    #print chooseBestFeatureToSplit(myDat)
+    #print createTree(myDat,lables)
+    lenses()
